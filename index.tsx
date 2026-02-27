8efb32db1b89d42962f05f171de658a2c22e4b3a
Diogo O. Lins <contato@goatech.com.br>
2026-02-27 05:37:26 -0300

    Fix: corrigindo versão

--- index.tsx
+++ index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { SpeedInsights } from '@vercel/speed-insights/next';
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -13,6 +11,5 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
   </React.StrictMode>
 );
