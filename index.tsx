2e7a6c03189ea0dae7ca2694d52cf5c133bcf75a
Diogo O. Lins <contato@goatech.com.br>
2026-02-27 05:35:00 -0300

    Fix: Adicionando insights da vercel

--- index.tsx
+++ index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -11,5 +13,6 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
